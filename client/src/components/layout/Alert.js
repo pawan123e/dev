ffc3fdd39c5ee44a48f723e61f15e79f18b14e08
unknown <pawanrealtek@gmail.com>
2020-06-22 22:34:38 +0530

    alert position has changed

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 
 const Alert = ({alerts}) => alerts !== null && alerts.length > 0 && alerts.map(alert => (
     <AlertWrap>
-    <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+    <div key={alert.id} className='alert'>
         {alert.msg}
     </div>
     </AlertWrap>
@@ -22,11 +22,29 @@ const mapStateToProps = state => ({
 export default connect(mapStateToProps)(Alert)
 
 const AlertWrap = styled.div`
-padding-top: 12vh;
-position: fixed;
-width: 60%;
-margin: auto;
+position: absolute;
+bottom: 5vh;
+left: 0;
+width: 98vw;
+height: 7vh;
+z-index: 12;
+.alert{
+    width: 35%;
+    margin-left: 1rem;
+    margin-bottom: 2rem;
+    padding: 0.7rem 1.5rem;
+    background: #323232;
+    color: white;
+    opacity: 1;
+    border-radius: 3px;
+    position: fixed;
+}
 @media(max-width: 500px) {
-    width: 80%;
+    bottom: 0;
+    .alert{
+        width: 100%;
+        height: 100%;
+        margin: 0;
+    }
 }
 `
